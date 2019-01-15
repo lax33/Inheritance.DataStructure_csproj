5176afbd201d060fa2255a0c765edbe1afbd50e7
lax33 <lax33@tut.by>
2019-01-15 13:03:13 +0300

    Добавьте файлы проекта.

---
+++ Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance.DataStructure
+{
+    public class Category
+    {
+        public Category(string a, MessageType messageType, MessageTopic messageTopic)
+        {
+
+        }
+    }
+}
